97cd6ce1481ee7f1af224f312a3ce2fd7f94da21
Valentin Bosch <val.bosch2001@gmail.com>
2026-03-11 12:03:56 +0100

    vev

--- train.sh
+++ train.sh
@@ -4,10 +4,10 @@
 #$ -o result.out
 #$ -N mil_train
 cd /beegfs/data/work/c-2iia/vb710264/optimus
-module load python
-source /beegfs/data/work/c-2iia/vb710264/optimus/optimus_lora/bin/activate
-export PYTHONPATH=/work/c-2iia/vb710264/optimus/optimus_lora/lib/python3.9/site-packages:$PYTHONPATH
-export MPLCONFIGDIR=/work/c-2iia/vb710264/.cache/matplotlib
+module load python/3.11/anaconda/2024.02
+bash
+source ~/.bashrc
+conda activate optimus_env
 
 cd /beegfs/data/work/c-2iia/vb710264/optimus/optima_lora
 python /beegfs/data/work/c-2iia/vb710264/optimus/optima_lora/tiles_dataset_extract.py
