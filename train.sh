fbcbab31ad8b08ecf699bef50fb4361fe9dc8fb9
Valentin Bosch <val.bosch2001@gmail.com>
2026-03-11 14:24:05 +0100

    up

--- train.sh
+++ train.sh
@@ -7,7 +7,7 @@ cd /beegfs/data/work/c-2iia/vb710264/optimus
 module load python/3.11/anaconda/2024.02
 bash
 source ~/.bashrc
-conda activate optimus_env
+conda activate optimus_venv
 
 cd /beegfs/data/work/c-2iia/vb710264/optimus/optima_lora
 python /beegfs/data/work/c-2iia/vb710264/optimus/optima_lora/tiles_dataset_extract.py
